3a8620af3eeda92a3aeb53cf394de11876acf4e7
Roland Odenore - jsboy <rolandodenore@gmail.com>
2025-09-24 21:20:52 +0100

    feat: add feature section

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import About from "./components/About"
+import Feature from "./components/Feature"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 
@@ -6,6 +8,8 @@ const App = () => {
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Navbar />
       <Hero />
+      <About />
+      <Feature />
     </main>
   )
 }
